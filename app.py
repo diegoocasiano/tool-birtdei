0c9fcfed5c1a8a58f6e3c2dfff936dc97d21d9db
Diego Casiano <dianlonso02@gmail.com>
2024-02-28 00:56:29 -0500

    add request to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from pyairtable import Api
